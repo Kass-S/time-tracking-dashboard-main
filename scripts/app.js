2c4e0de5f2d54053ca1367326a663c73a561c54b
Kass-S <kassiespacek101@gmail.com>
2024-12-03 15:59:51 -0800

    created even listeners

--- scripts/app.js
+++ scripts/app.js
@@ -14,3 +14,22 @@ let socialCurrent = document.getElementById('socialCurrent');
 let socialPrevious = document.getElementById('socialPrevious');
 let selfcareCurrent = document.getElementById('selfcareCurrent');
 let selfcarePrevious = document.getElementById('selfcarePrevious');
+
+
+dailyBtn.addEventListener('click', function(){
+    dailyBtn.className = 'day-week-month day-week-month-active'
+    weeklyBtn.className = 'day-week-month'
+    monthlyBtn.className = 'day-week-month'
+})
+
+weeklyBtn.addEventListener('click', function(){
+    dailyBtn.className = 'day-week-month'
+    weeklyBtn.className = 'day-week-month day-week-month-active'
+    monthlyBtn.className = 'day-week-month'
+})
+
+monthlyBtn.addEventListener('click', function(){
+    dailyBtn.className = 'day-week-month'
+    weeklyBtn.className = 'day-week-month'
+    monthlyBtn.className = 'day-week-month day-week-month-active'
+})
