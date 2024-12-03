efd72cb16b1436f88fbf764bc83113918a7bd8a2
Kass-S <kassiespacek101@gmail.com>
2024-12-03 15:54:18 -0800

    created elements are got them to js

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,15 @@ let dailyBtn = document.getElementById('dailyBtn');
 let weeklyBtn = document.getElementById('weeklyBtn');
 let monthlyBtn = document.getElementById('monthlyBtn');
 
+let workCurrent = document.getElementById('workCurrent');
+let workPrevious = document.getElementById('workPrevious');
+let playCurrent = document.getElementById('playCurrent');
+let playPrevious =document.getElementById('playPrevious');
+let studyCurrent = document.getElementById('studyCurrent');
+let studyPrevious = document.getElementById('studyPrevious');
+let exerciseCurrent = document.getElementById('exerciseCurrent');
+let exercisePrevious = document.getElementById('exercisePrevious');
+let socialCurrent = document.getElementById('socialCurrent');
+let socialPrevious = document.getElementById('socialPrevious');
+let selfcareCurrent = document.getElementById('selfcareCurrent');
+let selfcarePrevious = document.getElementById('selfcarePrevious');
